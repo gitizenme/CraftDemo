bf2f20dd8c38a190d069106973b8c58dfc370b97
Joe Chavez <joe@izen.me>
2018-06-29 17:30:18 -0700

    Project setup and initial tests...

---
+++ Project/src/test/java/demo/craft/RestApiTests.java
@@ -0,0 +1,26 @@
+package demo.craft;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RestApiTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        Assert.assertNotNull(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class));
+    }
+}
