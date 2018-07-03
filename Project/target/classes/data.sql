9a90b0103957289aa2eb37a6e34d1489473f57e1
Joe Chavez <joe@izen.me>
2018-07-02 18:29:07 -0700

    LDAP and SQL

---
+++ Project/target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, userid, username, created_at) VALUES (1, 'uid=ben,ou=people,dc=craftdemo,dc=org', 'ben', NOW());
+INSERT INTO users (id, userid, username, created_at) VALUES (2, 'uid=bob,ou=people,dc=craftdemo,dc=org', 'bob', NOW());
+INSERT INTO users (id, userid, username, created_at) VALUES (3, 'uid=joe,ou=otherpeople,dc=craftdemo,dc=org', 'joe', NOW());
+INSERT INTO users (id, userid, username, created_at) VALUES (4, 'cn=mouse\, jerry,ou=people,dc=craftdemo,dc=org', 'jerry', NOW());
+INSERT INTO users (id, userid, username, created_at) VALUES (5, 'cn=slash/guy,ou=people,dc=craftdemo,dc=org', 'slashguy', NOW());
+INSERT INTO users (id, userid, username, created_at) VALUES (6, 'cn=quote\"guy,ou=\"quoted people\",dc=craftdemo,dc=org', 'quoteguy', NOW());
+INSERT INTO users (id, userid, username, created_at) VALUES (7, 'space cadet,ou=space cadets,dc=craftdemo,dc=org', 'spacecadet', NOW());
+
